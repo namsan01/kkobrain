449f6c015fcad7ed2ffaea0a3a2654e09049eae6
namsan01 <namsan2934@gmail.com>
2024-03-26 12:21:02 +0900

    네이버 서치어드바이저 등록 테스트 2차

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,12 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <head></head>
+      <head>
+        <meta
+          name="naver-site-verification"
+          content="08fc601fdbc31a914928df033987d05c7f5caefd"
+        />
+      </head>
       <body>
         <noscript>You need to enable JavaScript to run this app.</noscript>
         <div id="root">{children}</div>
